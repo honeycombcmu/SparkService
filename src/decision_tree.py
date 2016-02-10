eb6d4e939123ed935847076f43660f78fbdf9ed5
wrj5518 <wrj5518@gmail.com>
2016-02-09 23:41:05 -0500

    add decision tree machine learning algorithm

---
+++ src/decision_tree.py
@@ -0,0 +1,32 @@
+from pyspark import SparkContext
+from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
+from pyspark.mllib.util import MLUtils
+from pyspark.mllib.evaluation import BinaryClassificationMetrics
+
+TRAINING_FILE = 'a3a.txt'
+TEST_FILE = 'a3a.t'
+
+# Load and parse the data file into an RDD of LabeledPoint.
+trainingData = MLUtils.loadLibSVMFile(sc, TRAINING_FILE)
+testData = MLUtils.loadLibSVMFile(sc, TEST_FILE)
+
+# Train a DecisionTree model.
+#  Empty categoricalFeaturesInfo indicates all features are continuous.
+model = DecisionTree.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},
+                                     impurity='gini', maxDepth=5, maxBins=32)
+
+# Evaluate model on test instances and compute test error
+predictions = model.predict(testData.map(lambda x: x.features))
+labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testData.count())
+metrics = BinaryClassificationMetrics(labelsAndPredictions)
+print metrics
+
+# Area under precision-recall curve
+print("Area under PR = %s" % metrics.areaUnderPR)
+
+# Area under ROC curve
+print("Area under ROC = %s" % metrics.areaUnderROC)
+print('Test Error = ' + str(testErr))
+# print('Learned classification tree model:')
+# print(model.toDebugString())
