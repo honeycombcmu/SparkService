89ebc113310bd0545548dc4ee467908a2a138bc8
Bangjie Liu <Jacob@Bangjies-MBP.home>
2016-03-02 01:42:31 -0500

    add updated refactor isotonic regression module

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/Users/Jacob/src/spark-1.6.0-bin-hadoop2.6/bin/spark-submit /Users/Jacob/SparkService/src/PySpark.py /Users/jacobliu/SparkService/data/sample_naive_bayes_data.txt IsotonicRegression
+/Users/Jacob/src/spark-1.6.0-bin-hadoop2.6/bin/spark-submit /Users/Jacob/SparkService/src/PySpark.py /Users/jacobliu/SparkService/data/sample_naive_bayes_data.txt GradientBoostedTrees
